1c09f8dc0d4371baf84ee72d844ccfc314109d74
formylove <gigglepilate@gmail.com>
2020-06-07 09:44:46 +0800

    as usual

--- src/main/java/com/datawiza/springMVC/demo/DemoApplication.java
+++ src/main/java/com/datawiza/springMVC/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.datawiza.azure.demo;
+package com.datawiza.springMVC.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/datawiza/springMVC/demo/DemoApplicationTests.java
+++ src/test/java/com/datawiza/springMVC/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.datawiza.azure.demo;
+package com.datawiza.springMVC.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
